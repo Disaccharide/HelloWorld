004f66ac9a0b16a02f859726141179b3f4609ae6
613S005\sse <1162124524@qq.com>
2016-09-21 21:30:28 +0800

    Finish Homework 2

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
-function main(){
-    var S:String = "HelloWorld";
-    console.log(S);
+function draw(n:Number){
+    for (var j :number= 1; j <=n; j++) {
+        var str :string= "";
+        for (var i:number = 1; i <= n - j; i++) {
+            str += " ";
+        }
+        for (var k :number= 1; k <= 2 * j - 1; k++) {
+            str += "*";
+        }
+        console.log(str);
+    }
 }
+
+var m:number = process.argv[2];
+draw(m);
